4f754ea144fcf52c79a421a39dd163f15091ccda
Juan David Peña <juandavidp76@hotmail.com>
2024-08-09 15:53:02 -0500

    fix: footer

--- src/app/footer/page.tsx
+++ src/app/footer/page.tsx
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (
-    <footer className='bg-black flex bottom-0 w-screen h-16 inset-0 items-center justify-between text-white sm:text-sm text-xs mt-20'>
+    <footer className='bg-black flex bottom-0 w-full h-16 inset-0 items-center justify-between text-white sm:text-sm text-xs mt-20 px-20'>
       <p>
         Copyright &copy; 2024
         <strong> E Floral Designs </strong>- Miami, Flo
         <a className='text-blue-sky'></a>
       </p>
-      <p>Powered by <a href='https://juandape.dev' target="blank"><span className="italic hover:font-bold">JuandapeDev</span></a></p>
+      <p>Powered by <a href='https://juandape.dev' target="blank"><span className="italic hover:text-ocre">JuandapeDev</span></a></p>
     </footer>
   );
 }
